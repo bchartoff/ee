9100dc592ef9fa20d2818f90a96eb3661db85996
Ben Chartoff <bchartoff@gmail.com>
2017-09-30 08:54:07 -0400

    replace swipe behavior with arrows

--- js/ee.js
+++ js/ee.js
@@ -108,49 +108,49 @@ $(document).ready(function()
      })
 });
 
-document.addEventListener('touchstart', handleTouchStart, false);        
-document.addEventListener('touchmove', handleTouchMove, false);
+// document.addEventListener('touchstart', handleTouchStart, false);        
+// document.addEventListener('touchmove', handleTouchMove, false);
 
-var xDown = null;                                                        
-var yDown = null;                                                        
+// var xDown = null;                                                        
+// var yDown = null;                                                        
 
-function handleTouchStart(evt) {                                         
-    xDown = evt.touches[0].clientX;                                      
-    yDown = evt.touches[0].clientY;                                      
-};                                                
+// function handleTouchStart(evt) {                                         
+//     xDown = evt.touches[0].clientX;                                      
+//     yDown = evt.touches[0].clientY;                                      
+// };                                                
 
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
+// function handleTouchMove(evt) {
+//     if ( ! xDown || ! yDown ) {
+//         return;
+//     }
 
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
+//     var xUp = evt.touches[0].clientX;                                    
+//     var yUp = evt.touches[0].clientY;
 
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
+//     var xDiff = xDown - xUp;
+//     var yDiff = yDown - yUp;
 
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 100 ) {
-            /* left swipe */
-			var pageNum = getPageNum()
-			showPage(pageNum - 1)
-        }
-        else if (xDiff < -100) {
-            /* right swipe */
-			var pageNum = getPageNum()
-			showPage(pageNum + 1)
-        }                       
-    } else {
-        // if ( yDiff > 0 ) {
-        //     /* up swipe */ 
-        // } else { 
-        //     /* down swipe */
-        // }
-        return;                                                              
-    }
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
-};
+//     if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+//         if ( xDiff > 100 ) {
+//             /* left swipe */
+// 			var pageNum = getPageNum()
+// 			showPage(pageNum - 1)
+//         }
+//         else if (xDiff < -100) {
+//             /* right swipe */
+// 			var pageNum = getPageNum()
+// 			showPage(pageNum + 1)
+//         }                       
+//     } else {
+//         // if ( yDiff > 0 ) {
+//         //     /* up swipe */ 
+//         // } else { 
+//         //     /* down swipe */
+//         // }
+//         return;                                                              
+//     }
+//     /* reset values */
+//     xDown = null;
+//     yDown = null;                                             
+// };
 
